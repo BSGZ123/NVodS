746721468d9e5766ef0cc32f7f1d34e9db125184
BsKPLu <bianshuiguangzhong@gmail.com>
2022-02-12 23:17:07 +0800

    梳理流程

---
+++ bskplu_api/api_user/src/main/java/com/bskplu/api_user/RemoteUserClient.java
@@ -0,0 +1,14 @@
+package com.bskplu.api_user;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * @InterfaceName: RemoteUserClient
+ * @Description: 用户API接口
+ * @Author BsKPLu
+ * @Date 2022/2/12
+ * @Version 1.1
+ */
+public interface RemoteUserClient {
+
+}
---
+++ bskplu_api/api_user/src/main/java/com/bskplu/api_user/entity/User.java
@@ -0,0 +1,25 @@
+package com.bskplu.api_user.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName: User
+ * @Description: 用户表
+ * @Author BsKPLu
+ * @Date 2022/2/12
+ * @Version 1.1
+ */
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+
+
+
+public class User implements Serializable {
+
+}
