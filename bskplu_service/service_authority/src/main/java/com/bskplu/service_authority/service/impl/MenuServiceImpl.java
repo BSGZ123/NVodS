7a259e41af6933b78d48656e1e04d93a017b359c
BsKPLu <bianshuiguangzhong@gmail.com>
2022-03-02 17:44:21 +0800

    service_authority 菜单服务实现类编写ing

--- bskplu_service/service_authority/src/main/java/com/bskplu/service_authority/service/impl/MenuServiceImpl.java
+++ bskplu_service/service_authority/src/main/java/com/bskplu/service_authority/service/impl/MenuServiceImpl.java
@@ -1,16 +1,21 @@
 package com.bskplu.service_authority.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.bskplu.common_utils.constant.Constants;
 import com.bskplu.common_utils.utils.ResponseResult;
 import com.bskplu.service_authority.entity.Menu;
+import com.bskplu.service_authority.entity.RoleMenu;
+import com.bskplu.service_authority.entity.User;
 import com.bskplu.service_authority.mapper.MenuMapper;
 import com.bskplu.service_authority.service.MenuService;
 import com.bskplu.service_authority.service.RoleMenuService;
 import com.bskplu.service_authority.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
@@ -29,19 +34,47 @@ import java.util.stream.Collectors;
 public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements MenuService {
     private final RoleMenuService roleMenuService;
     private final UserService userService;
+
+    /**
+     * 菜单列表
+     * @return
+     */
     @Override
     public List<Menu> getMenuList() {
-        return null;
+        return buildTreeMenuList(baseMapper.selectList(new LambdaQueryWrapper<Menu>()));
     }
 
+    /**
+     *通过id删除菜单 1.根据id查出子结点 2.批量删除
+     * @param id
+     * @return
+     */
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public ResponseResult removeMenuById(String id) {
-        return null;
+        return ResponseResult.toOk(baseMapper.deleteBatchIds(deleteChildrenMenu(id))>0);
     }
 
+    /**
+     * 根据角色Id查询菜单
+     * @param roleId
+     * @return
+     */
     @Override
     public List<Menu> selectMenuByRoleId(String roleId) {
-        return null;
+        // 1. 查询出全部的菜单
+        final List<Menu> menus = baseMapper.selectList(new LambdaQueryWrapper<Menu>());
+        final List<RoleMenu> roleMenus = roleMenuService.list(new LambdaQueryWrapper<RoleMenu>().eq(RoleMenu::getRoleId, roleId));
+        // 2. 根据角色关联表ID查询菜单Id
+        for (Menu menu : menus) {
+            for (RoleMenu roleMenu : roleMenus) {
+                if (menu.getId().equals(roleMenu.getPermissionId())) {
+                    //3.设为选中状态，方便下一步处理
+                    menu.setSelect(true);
+                }
+            }
+        }
+        return buildTreeMenuList(menus);
     }
 
     @Override
@@ -142,14 +175,34 @@ public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements Me
      * @return
      */
     private List<Menu> buildMenuList(Menu menu, List<Menu> menus) {
-        return menus.stream().filter(e -> e.getPid().equals(menu.getId()))
+        return menus.stream().filter(e -> e.getPid().equals(menu.getId()))//过滤查询List，获得符合的对象集合
                 .map(e -> {
-                    // 用于前端渲染
+                    // 用于前端渲染树形菜单
                     e.setLevel(menu.getLevel() + 1);
                     e.setChildMenuList(buildMenuList(e, menus));
                     return e;
                 }).collect(Collectors.toList());
     }
 
+    /**
+     * 删除子结点菜单
+     * @param id
+     * @return
+     */
+    private List<String> deleteChildrenMenu(String id){
+        return baseMapper.selectList(new LambdaQueryWrapper<Menu>().eq(Menu::getPid, id).select(Menu::getId)).stream()
+                .map(e -> {
+                    deleteChildrenMenu(e.getId());
+                    return e;
+                }).map(Menu::getId).collect(Collectors.toList());
+    }
 
+    private Boolean isSysAdmin(String userId){
+        User user=userService.getById(userId);
+
+        if(user != null && Constants.ADMIN.equalsIgnoreCase(user.getUsername())){
+            return true;
+        }
+        return false;
+    }
 }
