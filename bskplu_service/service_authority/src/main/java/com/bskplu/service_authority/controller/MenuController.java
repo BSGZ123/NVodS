6cf4e425fb86125b1dea80c5fc133c2566923b31
BsKPLu <bianshuiguangzhong@gmail.com>
2022-03-02 21:35:40 +0800

    service_authority 菜单控制类类编写ing

--- bskplu_service/service_authority/src/main/java/com/bskplu/service_authority/controller/MenuController.java
+++ bskplu_service/service_authority/src/main/java/com/bskplu/service_authority/controller/MenuController.java
@@ -1,5 +1,14 @@
 package com.bskplu.service_authority.controller;
 
+import com.bskplu.common_utils.utils.ResponseResult;
+import com.bskplu.service_authority.service.MenuService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @ClassName: MenuController
  * @Description: 菜单权限 前端控制类
@@ -7,5 +16,18 @@ package com.bskplu.service_authority.controller;
  * @Date 2022/3/1
  * @Version 1.1
  */
+@Api(tags = "菜单管理")
+@RestController
+@RequestMapping("/service_authority/admin/menu")
+@RequiredArgsConstructor
 public class MenuController {
+    private final MenuService menuService;
+
+    @ApiModelProperty(value = "获取菜单列表")
+    @PostMapping("/getMenuList")
+    public ResponseResult getMenuList(){
+        return ResponseResult.ok().data(menuService.getMenuList());
+    }
+
+
 }
