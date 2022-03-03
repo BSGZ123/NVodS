7778923512a6d3b52cf30950e3c5cfc1e961466b
BsKPLu <bianshuiguangzhong@gmail.com>
2022-03-03 11:30:44 +0800

    service_authority 菜单控制类完成

--- bskplu_service/service_authority/src/main/java/com/bskplu/service_authority/controller/MenuController.java
+++ bskplu_service/service_authority/src/main/java/com/bskplu/service_authority/controller/MenuController.java
@@ -1,13 +1,12 @@
 package com.bskplu.service_authority.controller;
 
 import com.bskplu.common_utils.utils.ResponseResult;
+import com.bskplu.service_authority.entity.Menu;
 import com.bskplu.service_authority.service.MenuService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @ClassName: MenuController
@@ -29,5 +28,34 @@ public class MenuController {
         return ResponseResult.ok().data(menuService.getMenuList());
     }
 
+    @ApiModelProperty(value = "根据ID删除所有子节点菜单")
+    @PostMapping("/removeMenuById/{id}")
+    public ResponseResult removeMenuById(@PathVariable String id){
+        return menuService.removeMenuById(id);
+    }
+
+    @ApiModelProperty(value = "根据角色查询菜单")
+    @GetMapping("/getMenuWithRoleId/{roleId}")
+    public ResponseResult getMenuWithRoleId(@PathVariable String roleId){
+        return ResponseResult.ok().data(menuService.selectMenuByRoleId(roleId));
+    }
+
+    @ApiModelProperty(value = "给用户角色赋予菜单权限")
+    @PostMapping("/doAssignRoleAuth/{roleId}/{menuIds}")
+    public ResponseResult doAssignRoleAuth(@PathVariable String roleId,@PathVariable String... menuIds){
+        return menuService.saveRoleMenuRelationShip(roleId, menuIds);
+    }
+
+    @ApiModelProperty(value = "保存菜单")
+    @PostMapping("/saveMenu")
+    public ResponseResult save(@RequestBody Menu menu){
+        return  ResponseResult.toOk(menuService.save(menu));
+    }
+
+    @ApiModelProperty(value = "更新菜单")
+    @PostMapping("/updateMenu")
+    public ResponseResult update(@RequestBody Menu menu){
+        return ResponseResult.toOk(menuService.updateById(menu));
+    }
 
 }
