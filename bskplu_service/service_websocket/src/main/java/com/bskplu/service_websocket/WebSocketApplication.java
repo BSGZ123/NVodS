2be176914ce4d9cce1724a9099f0bc7580702cc6
BsKPLu <bianshuiguangzhong@gmail.com>
2022-03-05 10:45:05 +0800

    service_websocket子模块

---
+++ bskplu_service/service_websocket/src/main/java/com/bskplu/service_websocket/WebSocketApplication.java
@@ -0,0 +1,16 @@
+package com.bskplu.service_websocket;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName: WebSocketApplication
+ * @Description: service_websocket服务启动类
+ * @Author BsKPLu
+ * @Date 2022/3/5
+ * @Version 1.1
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class WebSocketApplication {
+}
