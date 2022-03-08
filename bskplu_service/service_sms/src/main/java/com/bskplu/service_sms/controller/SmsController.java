727c1a2a34c7d84902cfc4c1e804fe5100706a35
BsKPLu <bianshuiguangzhong@gmail.com>
2022-03-08 16:22:05 +0800

    service_sms完成

--- bskplu_service/service_sms/src/main/java/com/bskplu/service_sms/controller/SmsController.java
+++ bskplu_service/service_sms/src/main/java/com/bskplu/service_sms/controller/SmsController.java
@@ -3,11 +3,17 @@ package com.bskplu.service_sms.controller;
 import com.bskplu.common_utils.utils.ResponseResult;
 import com.bskplu.service_base.exception.BusinessException;
 import com.bskplu.service_base.utils.text.PhoneUtils;
+import com.bskplu.service_base.utils.text.StringUtils;
 import com.bskplu.service_sms.service.SmsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @ClassName: SmsController
@@ -28,7 +34,39 @@ public class SmsController {
         if(!PhoneUtils.isPhone(phone)){
             throw new BusinessException("该手机号不符合规范！");
         }
-        return null;
+
+        //查询redis中是否有跟手机号相对应的验证码 手机号当作key
+        String code=redisTemplate.opsForValue().get(phone);
+        if(!StringUtils.isEmpty(code)){
+            //已经发送过了，未过期
+            return ResponseResult.ok();
+        }
+        code=randomCode();
+        Map<String,Object> param=new HashMap<>();
+        param.put("code",code);
+        Boolean isSend=smsService.send(phone,param);
+
+        //发送成功
+        if(isSend){
+            //将验证码存入redis中，5min过期时间
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
+            return ResponseResult.ok();
+        }else {
+            return ResponseResult.error().message("发送短信失败");
+        }
+    }
+
+    /**
+     * 生成随机4位验证码
+     * @return
+     */
+    public String randomCode () {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            int s = new Random().nextInt(10);
+            sb.append(s);
+        }
+        return sb.toString();
     }
 
 }
