ee83a58ef7b1d61eb52bdf2f141908128070a847
BsKPLu <bianshuiguangzhong@gmail.com>
2022-03-15 23:04:26 +0800

    service_video 编写服务类ing

--- bskplu_service/service_video/src/main/java/com/bskplu/service_video/service/impl/ChapterServiceImpl.java
+++ bskplu_service/service_video/src/main/java/com/bskplu/service_video/service/impl/ChapterServiceImpl.java
@@ -5,10 +5,17 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.bskplu.api_video.entity.Chapter;
 import com.bskplu.api_video.entity.ContentVideo;
 import com.bskplu.api_video.entity.vo.ChapterVO;
+import com.bskplu.api_video.entity.vo.ContentVideoVO;
+import com.bskplu.common_utils.utils.ResultCode;
+import com.bskplu.service_base.exception.BusinessException;
 import com.bskplu.service_video.mapper.ChapterMapper;
 import com.bskplu.service_video.service.ChapterService;
+import com.bskplu.service_video.service.ContentVideoService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,6 +29,9 @@ import java.util.List;
 public class ChapterServiceImpl extends ServiceImpl<ChapterMapper, Chapter> implements ChapterService {
 
 
+    @Autowired
+    private ContentVideoService contentVideoService;
+
     @Override
     public List<ChapterVO> getChapterContentVideo(String contentId) {
         final LambdaQueryWrapper<Chapter> queryWrapper=new LambdaQueryWrapper<>();
@@ -31,22 +41,49 @@ public class ChapterServiceImpl extends ServiceImpl<ChapterMapper, Chapter> impl
         //根据课程id 获取视频内容章节
         final LambdaQueryWrapper<ContentVideo> contentVideoLambdaQueryWrapper=new LambdaQueryWrapper<>();
         contentVideoLambdaQueryWrapper.eq(ContentVideo::getContentId,contentId);
-        //final List<ContentVideo> contentVideos=
-        return null;
+        final List<ContentVideo> contentVideos=contentVideoService.list(contentVideoLambdaQueryWrapper);
+
+        //组装章节数据
+        final ArrayList<ChapterVO> list=new ArrayList<ChapterVO>(16);
+
+        //组装数据
+        for (Chapter chapter:chapters){
+            final ChapterVO chapterVO=new ChapterVO();
+            BeanUtils.copyProperties(chapter,chapterVO);
+
+            //小节
+            final ArrayList<ContentVideoVO> a1=new ArrayList<ContentVideoVO>();
+            for (ContentVideo video:contentVideos){
+                if (video.getChapterId().equals(chapterVO.getId())){
+                    ContentVideoVO vo=new ContentVideoVO();
+                    BeanUtils.copyProperties(video,vo);
+                    a1.add(vo);
+                }
+            }
+            chapterVO.setChildren(a1);
+            list.add(chapterVO);
+        }
+        return list;
     }
 
     @Override
     public Boolean deleteChapterById(String id) {
-        return null;
+        if (contentVideoService.getCountByChapterId(id)){
+            throw new BusinessException(ResultCode.ERROR,"该分章节下存在小节，请先删除小节");
+        }
+        Integer result=baseMapper.deleteById(id);
+        return result!=null && result>0;
     }
 
     @Override
     public Integer getContentOrVideoSortMax(Integer type) {
-        return null;
+        return this.baseMapper.getContentOrVideoSortMax(type);
     }
 
     @Override
     public Boolean deleteChapterByContentId(String id) {
-        return null;
+        final LambdaQueryWrapper<Chapter> wrapper=new LambdaQueryWrapper<>();
+        this.baseMapper.delete(wrapper.eq(Chapter::getContentId,id));
+        return false;
     }
 }
