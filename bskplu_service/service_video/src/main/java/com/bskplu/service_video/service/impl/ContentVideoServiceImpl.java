ca9334391e3cb4f63b840a43c84c7027207b8306
BsKPLu <bianshuiguangzhong@gmail.com>
2022-03-16 18:35:52 +0800

    service_video 编写服务类ing

--- bskplu_service/service_video/src/main/java/com/bskplu/service_video/service/impl/ContentVideoServiceImpl.java
+++ bskplu_service/service_video/src/main/java/com/bskplu/service_video/service/impl/ContentVideoServiceImpl.java
@@ -1,14 +1,22 @@
 package com.bskplu.service_video.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.bskplu.api_video.entity.ContentVideo;
 import com.bskplu.api_video.entity.vo.ContentVideoInfoVO;
 import com.bskplu.api_video.entity.vo.ContentVideoVO;
+import com.bskplu.api_vod.VodClient;
+import com.bskplu.common_utils.utils.ResponseResult;
+import com.bskplu.common_utils.utils.ResultCode;
+import com.bskplu.service_base.exception.BusinessException;
+import com.bskplu.service_base.utils.text.StringUtils;
 import com.bskplu.service_video.mapper.ContentVideoMapper;
 import com.bskplu.service_video.service.ContentVideoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @ClassName: ContentVideoServiceImpl
@@ -21,24 +29,54 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class ContentVideoServiceImpl extends ServiceImpl<ContentVideoMapper, ContentVideo> implements ContentVideoService {
+
+    private final VodClient vodClient;
+
     @Override
+    //获得小节章节数
     public boolean getCountByChapterId(String id) {
-        return false;
+        final LambdaQueryWrapper<ContentVideo> wrapper=new LambdaQueryWrapper<>();
+        wrapper.eq(ContentVideo::getChapterId,id);
+        return baseMapper.selectCount(wrapper)>0;
     }
 
+    /**
+     * 从前端获取vo添加保存到数据库
+     * @param contentVideoVO
+     * @return
+     */
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public boolean addContentVideo(ContentVideoVO contentVideoVO) {
-        return false;
+        final ContentVideo contentVideo=new ContentVideo();
+        BeanUtils.copyProperties(contentVideoVO,contentVideo);
+        return this.save(contentVideo);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public boolean updateContentVideo(ContentVideoInfoVO contentVideoInfoVO) {
-        return false;
+        final ContentVideo contentVideo=new ContentVideo();
+        BeanUtils.copyProperties(contentVideoInfoVO,contentVideo);
+        return this.updateById(contentVideo);
     }
 
     @Override
     public boolean deleteContentVideoWithById(String id) {
-        return false;
+        //根据id获取必要信息
+        final ContentVideo contentVideo=this.baseMapper.selectById(id);
+        final String videoSourceId=contentVideo.getVideoSourceId();
+
+        if(!StringUtils.isEmpty(videoSourceId)){
+            final ResponseResult responseResult=vodClient.removeVideo(videoSourceId);
+            if(!responseResult.getSuccess()){
+                throw new BusinessException(ResultCode.ERROR,"调用删除远程视频失败！");
+            }
+            log.info("小节视频删除成功:"+responseResult);
+        }else{
+            log.error("视频资源不存在！视频删除失败！:{0}"+id);
+        }
+        return this.baseMapper.deleteById(id) > 0;
     }
 
     @Override
