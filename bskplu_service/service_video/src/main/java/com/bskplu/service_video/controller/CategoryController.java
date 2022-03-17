a6a349041722805c83240cf9929c5fe5859fe277
BsKPLu <bianshuiguangzhong@gmail.com>
2022-03-17 21:25:06 +0800

    service_video 编写控制类ing

--- bskplu_service/service_video/src/main/java/com/bskplu/service_video/controller/CategoryController.java
+++ bskplu_service/service_video/src/main/java/com/bskplu/service_video/controller/CategoryController.java
@@ -1,11 +1,16 @@
 package com.bskplu.service_video.controller;
 
+import com.bskplu.common_utils.utils.ResponseResult;
 import com.bskplu.service_video.service.CategoryService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @ClassName: CategoryController
@@ -22,6 +27,20 @@ public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    @ApiOperation(value = "获取所有分类信息")
+    @GetMapping("/getAllCategory")
+    public ResponseResult getAllCategory(){
+        return ResponseResult.ok().data(categoryService.getTreeCategory());
+    }
+
+
+    @ApiOperation(value = "导入新增分类信息")
+    @PostMapping("/addCategory")
+    public ResponseResult addCategory(MultipartFile multipartFile){
+        categoryService.saveCategory(multipartFile);
+        return ResponseResult.ok();
+    }
 
 
 }
--- bskplu_service/service_video/src/main/java/com/bskplu/service_video/controller/ChapterController.java
+++ bskplu_service/service_video/src/main/java/com/bskplu/service_video/controller/ChapterController.java
@@ -1,5 +1,14 @@
 package com.bskplu.service_video.controller;
 
+import com.bskplu.common_utils.utils.ResponseResult;
+import com.bskplu.service_video.service.ChapterService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @ClassName: ChapterController
  * @Description: 小节控制类
@@ -7,5 +16,17 @@ package com.bskplu.service_video.controller;
  * @Date 2022/3/16
  * @Version 1.1
  */
+@RestController
+@RequestMapping("/service_video/chapter")
+@RequiredArgsConstructor
 public class ChapterController {
+    private final ChapterService chapterService;
+
+    @ApiOperation(value = "获取章节列表")
+    @GetMapping("getChapterContentVideo/{contentId}")
+    public ResponseResult getChapterContentVideo(@PathVariable String contentId){
+        return ResponseResult.ok().data(chapterService.getChapterContentVideo(contentId));
+    }
+
+
 }
