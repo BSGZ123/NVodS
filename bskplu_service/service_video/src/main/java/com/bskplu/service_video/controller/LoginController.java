403efb6462176a5bb0e1b11063743fe1f026b5a4
BsKPLu <bianshuiguangzhong@gmail.com>
2022-03-17 16:15:17 +0800

    service_video 编写控制类ing

--- bskplu_service/service_video/src/main/java/com/bskplu/service_video/controller/LoginController.java
+++ bskplu_service/service_video/src/main/java/com/bskplu/service_video/controller/LoginController.java
@@ -1,5 +1,11 @@
 package com.bskplu.service_video.controller;
 
+import com.bskplu.common_utils.utils.ResponseResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @ClassName: LoginController
  * @Description: 登录控制类
@@ -7,5 +13,22 @@ package com.bskplu.service_video.controller;
  * @Date 2022/3/16
  * @Version 1.1
  */
+@RestController
+@RequestMapping("/service_video/user")
 public class LoginController {
+
+    @PostMapping("/login")
+    public ResponseResult login(){
+        return ResponseResult.ok().data("token","admin-token");
+    }
+
+    @GetMapping("/info")
+    public ResponseResult info () {
+        return ResponseResult.ok()
+                .data("roles", "[admin]")
+                .data("name", "fmjava")
+                .data("avatar", "http://129.211.209.210/torture.gif");
+    }
+
+
 }
