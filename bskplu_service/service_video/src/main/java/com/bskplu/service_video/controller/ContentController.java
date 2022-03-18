220c668bcd542f1142f48c6bd2657db53b2d3e4a
BsKPLu <bianshuiguangzhong@gmail.com>
2022-03-18 12:52:48 +0800

    service_video 编写ContentController控制类

--- bskplu_service/service_video/src/main/java/com/bskplu/service_video/controller/ContentController.java
+++ bskplu_service/service_video/src/main/java/com/bskplu/service_video/controller/ContentController.java
@@ -1,5 +1,15 @@
 package com.bskplu.service_video.controller;
 
+import com.bskplu.api_pay.RemotePayOrderClient;
+import com.bskplu.api_video.entity.vo.ContentVo;
+import com.bskplu.common_utils.utils.ResponseResult;
+import com.bskplu.service_video.service.ChapterService;
+import com.bskplu.service_video.service.ContentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * @ClassName: ContentController
  * @Description: 视频内容控制类
@@ -7,5 +17,24 @@ package com.bskplu.service_video.controller;
  * @Date 2022/3/16
  * @Version 1.1
  */
+@Api(tags = "作品管理")
+@RestController
+@RequestMapping("/service_video/content")
+@RequiredArgsConstructor
 public class ContentController {
+    private final ContentService contentService;
+    private final ChapterService chapterService;
+    private final RemotePayOrderClient remotePayOrderClient;
+
+    @ApiOperation(value = "新增作品内容")
+    @PostMapping("/addContentInfo")
+    public ResponseResult addContentInfo(ContentVo contentVo){
+        return ResponseResult.ok().data("contentId",contentService.addContentInfo(contentVo));
+    }
+
+    @ApiOperation(value = "根据id获取作品详细内容信息")
+    @GetMapping("/getContentWhitInfo/{id}")
+    public ResponseResult getContentWhitInfo(@PathVariable String id){
+        return ResponseResult.ok().data(contentService.getContentWhitInfo(id));
+    }
 }
